2e1b8995e91be673bbe9c2d983ab2ca8b1195ccc
laurabiggins <laurabiggins10@gmail.com>
2024-09-20 11:37:06 +0100

    small refactoring

--- R/mod_scatter.R
+++ R/mod_scatter.R
@@ -3,6 +3,17 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 
+update_geom_defaults("point", 
+   aes(
+      fill   = "seagreen", 
+      colour = "black", 
+      shape  = 21, 
+      size   = 4 , 
+      stroke = 0.5
+      )
+)
+
+
 mod_scatter_ui <- function(id){
   
   ns <- NS(id)
@@ -22,13 +33,13 @@ mod_scatter_server <- function(id, tbl_list, show_lines, point_colour, highlight
       
       output$scatter <- renderPlotly(gg_plot())
       
-     # highlight_colours <- c(N = "#00000000", Y = "red")
       highlight_colours <- c(N = "transparent", Y = "red")
 
       # extract the dataset from the nested list of tibbles
       # get i from id
       i <- as.numeric(gsub(id, pattern = "tissue", replacement = ""))
       dataset <- reactive(tbl_list[i,2][[1]][[1]])
+      
       
       gg_plot <- reactive({
 
@@ -51,70 +62,72 @@ mod_scatter_server <- function(id, tbl_list, show_lines, point_colour, highlight
         
       })
       
+      
+      selected_data <- reactive({
+        
+        filter(dataset(), Gene == gene_to_plot())
+        
+      })
+      
       base_plot <- reactive({
         
-        dataset() |>
-          filter(Gene == gene_to_plot()) |>
+        # dataset() |>
+        #   filter(Gene == gene_to_plot()) |>
+        selected_data() |>
           ggplot(aes(x = age, y = oxi_percent, key = residue)) +
-          geom_point_display() +
+          #geom_point_display() +
           ggtitle(gene_to_plot())
         
       })
       
+      
+      # assembled plot
       complete_plot <- reactive({
         
-        completed_plot <- base_plot()
+        completed_plot <- base_plot() +
+          geom_point_display()
         
-        if (show_lines()){
-          completed_plot <- completed_plot +
-            geom_line(
-              aes(group = residue), 
-              linewidth = 0.5, 
-              linetype  = "dashed", 
-              colour    = "grey"
-            ) +
-            geom_point_display()
+        if (nrow(selected_data()) == 0) {
+          completed_plot <- base_plot() + 
+            annotate(geom="text", x = 1, y = 1, label = "no data")
+        } else {
+        
+          if (show_lines()){
+            completed_plot <- completed_plot +
+              geom_line(
+                aes(group = residue), 
+                linewidth = 0.5, 
+                linetype  = "dashed", 
+                colour    = "grey"
+              ) +
+              geom_point_display()
+          }
+          
+          if ( highlight() != "None" ) {
+            completed_plot <- completed_plot +
+              geom_point_display() +
+              geom_point(
+                aes(colour = .data[[highlight()]]), 
+                fill = "transparent",
+                stroke = 1.5
+              ) +
+              scale_colour_manual(values = highlight_colours)
+          }
         }
-        
-        if ( highlight() != "None" ) {
-          completed_plot <- completed_plot +
-            geom_point(
-              aes(colour = .data[[highlight()]]), 
-              size   = 4, 
-              shape  = 21, 
-              stroke = 1.5
-            ) +
-            scale_colour_manual(values = highlight_colours)
-        }
-        
         return( completed_plot )
         
       })
       
-      
+      # get the point colours
       geom_point_display <- reactive({
         
         if (point_colour() == "residue") {
           return (
-            geom_point(
-              aes(fill = residue, colour = residue), 
-              shape  = 21, 
-              size   = 4, 
-              stroke = 0.5
-            )
+            geom_point(aes(fill = residue, colour = residue))
           )
-        } else {
-          return (
-            geom_point(
-              fill   = "black", 
-              colour = "black", 
-              shape  = 21, 
-              size   = 4 , 
-              stroke = 0.5
-            )
-          )
-        }
+        } else return (geom_point())
       })
+      
       
       top_ptp <- reactive({
        dataset() |>
